2db0e08cc949f2eb488eaa90962b3ed965698b14
Jeremiah <jeremiah@localhost.localdomain>
2013-10-02 09:27:32 -0700

    update python installer setup.py

--- upstream/setup.py
+++ upstream/setup.py
@@ -17,26 +17,26 @@ for x in glob.glob("nvidia-installer-hooks/*"):
     os.chmod(x, 0o755)
 
 setup(
-    name="ubuntu-drivers-common",
-    author="Alberto Milone",
-    author_email="albertomilone@alice.it",
-    maintainer="Alberto Milone",
-    maintainer_email="albertomilone@alice.it",
-    url="http://www.albertomilone.com",
+    name="pharlap-common",
+    author="Korora Project",
+    author_email="dev@kororaproject.org",
+    maintainer="Korora Project",
+    maintainer_email="dev@kororaproject.org",
+    url="http://kororaproject.org/",
     license="gpl",
-    description="Detect and install additional Ubuntu driver packages",
-    packages=["NvidiaDetector", "Quirks", "UbuntuDrivers"],
-    data_files=[("/usr/share/ubuntu-drivers-common/", ["share/obsolete", "share/fake-devices-wrapper"]),
-                ("/var/lib/ubuntu-drivers-common/", []),
+    description="Detect and install additional Korora akmod driver packages",
+    packages=["NvidiaDetector", "Quirks", "Pharlap"],
+    data_files=[("/usr/share/pharlap-common/", ["share/obsolete", "share/fake-devices-wrapper"]),
+                ("/var/lib/pharlap-common/", []),
                 ("/etc/", []),
-                ("/usr/share/ubuntu-drivers-common/quirks", glob.glob("quirks/*")),
-                ("/usr/share/ubuntu-drivers-common/detect", glob.glob("detect-plugins/*")),
-                ("/usr/share/doc/ubuntu-drivers-common", ['README']),
+                ("/usr/share/pharlap-common/quirks", glob.glob("quirks/*")),
+                ("/usr/share/pharlap-common/detect", glob.glob("detect-plugins/*")),
+                ("/usr/share/doc/pharlap-common", ['README']),
                 ("/usr/lib/nvidia/", glob.glob("nvidia-installer-hooks/*")),
                 ("/usr/lib/ubiquity/target-config", glob.glob("ubiquity/target-config/*")),
                ] + extra_data,
-    scripts=["nvidia-detector", "quirks-handler", "ubuntu-drivers"],
+    scripts=["nvidia-detector", "quirks-handler", "pharlap"],
     entry_points="""[packagekit.apt.plugins]
-what_provides=UbuntuDrivers.PackageKit:what_provides
+what_provides=Pharlap.PackageKit:what_provides
 """,
 )
